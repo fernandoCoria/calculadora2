89f8591ea1ddffee5b6fdb558dcb5de48d78c82e
Fernando Coria <f.a.coria@gmail.com>
2022-06-29 20:38:31 -0300

    aniadido de datos en pantalla

---
+++ scripts.js
@@ -0,0 +1,12 @@
+const display =document.querySelector('.display').innerHTML= 2345
+
+function escribir(valor ){
+    return document.querySelector('.display').innerHTML+=valor 
+        
+}
+
+function borrarTodo(){
+    return document.querySelector('.display').innerHTML= '0'
+
+}
+
